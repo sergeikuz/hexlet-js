9af2d09158fa73945633f601f9e108791c8e1f60
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2025-09-22 15:37:36 +0300

    create end point to the project

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import _ from "lodash";
+import myFunction from './src/index.js';
 
-console.log("Hello, Hexlet!");
-console.log(_.last(["one", "two"]));
+myFunction();
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import _ from 'lodash'
+
+const myFunction = () => {
+  console.log("Hello, Hexlet!");
+  console.log(_.last(["one", "two"]));
+};
+
+export default myFunction;
